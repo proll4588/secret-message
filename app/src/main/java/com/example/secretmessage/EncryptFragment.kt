68392ffde61fa9c5c5c1e99ba00376dd6b6d9b8c
Kostya <prol4588@gmail.com>
2023-10-21 08:30:15 +0700

    add encrypt fragment

--- app/src/main/java/com/example/secretmessage/EncryptFragment.kt
+++ app/src/main/java/com/example/secretmessage/EncryptFragment.kt
@@ -1,12 +1,10 @@
 package com.example.secretmessage
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MessageFragment.newInstance] factory method to
+ * Use the [EncryptFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MessageFragment : Fragment() {
+class EncryptFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,14 +34,7 @@ class MessageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_message, container, false)
-
-        val nextButton = view.findViewById<Button>(R.id.next)
-        nextButton.setOnClickListener {
-            view.findNavController()
-                .navigate(R.id.action_messageFragment_to_encryptFragment)
-        }
-        return view
+        return inflater.inflate(R.layout.fragment_encrypt, container, false)
     }
 
     companion object {
@@ -53,12 +44,12 @@ class MessageFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MessageFragment.
+         * @return A new instance of fragment EncryptFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MessageFragment().apply {
+            EncryptFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
